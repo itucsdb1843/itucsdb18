be5ee7875c12f824d92ae981c678a868ca9c2a39
bbuyukozdb <buyukozbora@gmail.com>
2018-11-21 05:41:37 -0500

    shared commit trial

--- views.py
+++ views.py
@@ -4,3 +4,5 @@ def home_page():
     return render_template('pages/index.html')
 def events_page():
     return render_template('pages/events.html')
+def results_page():
+    return render_template('pages/results.html')
