6beea0143a067db085161b24385084fff909e6bd
buyukoz17 <buyukoz17@itu.edu.tr>
2018-12-16 21:23:26 -0500

    some of the url rules and login manager

--- server.py
+++ server.py
@@ -1,22 +1,55 @@
-from flask import Flask, render_template
-from datetime import datetime
+import os
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+
+
+
+from models.user import Anonymous
 import views
-#app = Flask(__name__)
 
-
-#@app.route("/")
-#def home_page():
-#    return "ello"
 
 def create_app():
+
     app = Flask(__name__)
+    
     app.add_url_rule('/', 'index', view_func=views.home_page)
-    app.add_url_rule('/events','events', view_func=views.events_page)
+
+    app.add_url_rule('/events','events', view_func=views.events_page, methods = ["GET","POST"])
+
+    app.add_url_rule('/register','register', view_func=views.register_page, methods = ["GET","POST"])
+        
+    app.add_url_rule('/university_rankings', 'university_rankings', view_func=views.university_rankings_page, methods = ["GET","POST"])
+
+    app.add_url_rule('/upload_university_photo', 'upload_photo', view_func=views.upload_university_photo_page, methods = ["GET","POST"])
+
+    
     return app
 
 
 app = create_app()
 
+app.config.from_pyfile('config.py')
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.anonymous_user = Anonymous
+
+@login_manager.user_loader
+def load_user(user_id):
+    from database import usersTable
+    users_table = usersTable()
+    user = users_table.getUserObjectByID(user_id)
+    return user
+
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+
+    app.run()
+
+# implementation notes:
+# For method names, camelCase is used.
+# For variable names, under_score is used.
