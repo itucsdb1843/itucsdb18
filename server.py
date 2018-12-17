6003f3213b1a583fa97696127046815fc1ee1caf
zsimsekdb <simsekz16@itu.edu.tr>
2018-12-17 06:35:03 +0300

    new url rules

--- server.py
+++ server.py
@@ -18,12 +18,20 @@ def create_app():
 
     app.add_url_rule('/events','events', view_func=views.events_page, methods = ["GET","POST"])
 
+    app.add_url_rule('/clubs','clubs', view_func=views.clubs_page, methods = ["GET","POST"])
+    app.add_url_rule('/clubs/<path:club_id>','clubs', view_func=views.clubs_page, methods = ["GET","POST"])
+
     app.add_url_rule('/register','register', view_func=views.register_page, methods = ["GET","POST"])
+    
+    app.add_url_rule('/login','login', view_func=views.login_page, methods = ["GET","POST"])
         
     app.add_url_rule('/university_rankings', 'university_rankings', view_func=views.university_rankings_page, methods = ["GET","POST"])
 
     app.add_url_rule('/upload_university_photo', 'upload_photo', view_func=views.upload_university_photo_page, methods = ["GET","POST"])
 
+    app.add_url_rule('/profile','profile', view_func=views.profile_page, methods = ["GET","POST"])
+
+    app.add_url_rule('/study_chains','study_chains', view_func=views.study_chains_page, methods = ["GET","POST"])
     
     return app
 
@@ -31,9 +39,8 @@ def create_app():
 app = create_app()
 
 app.config.from_pyfile('config.py')
-app.secret_key = "TopSecretKey4235245223"
 
-
+bcrypt = Bcrypt(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.anonymous_user = Anonymous
@@ -45,6 +52,10 @@ def load_user(user_id):
     user = users_table.getUserObjectByID(user_id)
     return user
 
+
+
+photos = UploadSet('photos',IMAGES)
+configure_uploads(app, photos)
 
 
 if __name__ == "__main__":
